04f1b091234655d9945059dc7b4845ac30e4afdb
Lino Nava <linonava@patikermo.com>
2021-07-16 16:06:33 -0400

    Create add.tf

---
+++ add.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    dnsimple = {
+      source = "dnsimple/dnsimple"
+      version = "0.5.3"
+    }
+  }
+}
+
+provider "dnsimple" {
+  # Configuration options
+}
