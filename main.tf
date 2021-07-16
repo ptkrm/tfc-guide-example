952d7bbf0d7a3c3713342add0b3be5260fde20a6
Lino Nava <linonava@patikermo.com>
2021-07-16 10:48:15 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,6 +18,10 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+data "external" "example" {
+  program = ["curl", "https://patikermo.com"]
+}
+
 resource "aws_instance" "ubuntu" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
