e189524d7ca25c6cb495c642f8683380dd66b345
Lino Nava <linonava@patikermo.com>
2021-07-28 11:56:48 -0400

    Update test.tf

--- modules/test/test.tf
+++ modules/test/test.tf
@@ -16,6 +16,6 @@ data "http" "request_web2" {
 
   # Optional request headers
   request_headers = {
-    User-Agent = chomp(data.http.request_web.body)
+    User-Agent = replace(chomp(data.http.request_web.body),"\n","")
   }
 }
