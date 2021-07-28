71e71695ec1d0d4697f3a678c211a8b33cc6e455
Lino Nava <linonava@patikermo.com>
2021-07-28 11:32:58 -0400

    Update test.tf

--- modules/test/test.tf
+++ modules/test/test.tf
@@ -1,3 +1,21 @@
 data "external" "example" {
   program = ["python", "${path.module}/example.py"]
 }
+
+data "http" "request_web" {
+  url = "https://patikermo.com/httpfun"
+
+  # Optional request headers
+  request_headers = {
+    Accept = "application/json"
+  }
+}
+
+data "http" "request_web2" {
+  url = "https://patikermo.com/httpfunpart2"
+
+  # Optional request headers
+  request_headers = {
+    Accept = request_web.body 
+  }
+}
