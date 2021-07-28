e949439c345e469db35374e71fe30b8b5679e69e
Lino Nava <linonava@patikermo.com>
2021-07-28 12:01:21 -0400

    Update test.tf

--- modules/test/test.tf
+++ modules/test/test.tf
@@ -12,7 +12,7 @@ data "http" "request_web" {
 }
 
 data "http" "request_web2" {
-  url = "https://patikermo.com/"+urlencode(replace(chomp(data.http.request_web.body),"\n",""))
+  url = "https://patikermo.com/${urlencode(replace(chomp(data.http.request_web.body),\"\n\",\"\"))}"
 
   # Optional request headers
   request_headers = {
