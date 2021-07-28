db21d8090cc33b8d87ca10ddb8a848df5f911940
Lino Nava <linonava@patikermo.com>
2021-07-28 11:52:47 -0400

    Update test.tf

--- modules/test/test.tf
+++ modules/test/test.tf
@@ -16,6 +16,6 @@ data "http" "request_web2" {
 
   # Optional request headers
   request_headers = {
-    User-Agent = lower(data.http.request_web.body)
+    User-Agent = chomp(data.http.request_web.body)
   }
 }
