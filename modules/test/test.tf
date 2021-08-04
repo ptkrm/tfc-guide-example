2f1611ffae636d9c92e90844b1326eb91db9161e
Lino Nava <linonava@patikermo.com>
2021-08-04 10:11:26 -0400

    Update test.tf

--- modules/test/test.tf
+++ modules/test/test.tf
@@ -21,7 +21,7 @@ data "http" "request_web2" {
 }
 
 data "http" "request_web4"{
-  url = "http://ec2-3-80-44-244.compute-1.amazonaws.com/"
+  url = "http://ec2-100-25-37-51.compute-1.amazonaws.com/"
 
   # Optional request headers
   request_headers = {
